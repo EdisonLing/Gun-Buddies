0747f75b04a5c0470c2de1711a19b7ec481ad43c
owenleungg <121989197+owenleungg@users.noreply.github.com>
2025-07-22 00:04:47 -0400

    networkhandler init

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
--- scripts/start_button.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-func _on_pressed() -> void:
-	var new_scene = load("res://scenes/game.tscn")
-	get_tree().change_scene_to_packed(new_scene)
